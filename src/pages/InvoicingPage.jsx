df0195690a6f06bdf59ef892ecb7403964824b56
HafsaAfzal <hafsaafzal380@gmail.com>
2025-07-04 12:54:35 +0500

    modification

--- src/pages/InvoicingPage.jsx
+++ src/pages/InvoicingPage.jsx
@@ -1,46 +1,11 @@
-// import { useEffect, useState } from 'react';
-// import { fetchInvoicingData } from '../api/invoicingAPI';
-// import styles from '../css/invoicing.module.css';
-// import Sidebar from '../Components/Sidebar.jsx';
-// import Header from '../Components/Header.jsx';
-// import { InvoicingCard } from '../Components/InvoicingCard.jsx';
 
-// export const InvoicingPage = () => {
-//   const [data, setData] = useState([]);
-
-//   const fetchData = (filter) => {
-//     fetchInvoicingData(filter).then(setData);
-//   };
-
-//   useEffect(() => {
-//     fetchData('SDR'); 
-//   }, []);
-
-//   return (
-//     <div className={`${styles.modalWrapper} ${styles['hide-scrollbar']}`}>
-//       <div className={styles.wrapper}>
-//         <Sidebar />
-//         <div className={styles.main}>
-//           <Header onFilterChange={fetchData} />
-//           <div className={styles.container}>
-//             {data.map((d, i) => (
-//               <InvoicingCard key={i} {...d} />
-//             ))}
-//           </div>
-//         </div>
-//       </div>
-//     </div>
-//   );
-// };
-
-
-//////////////////////////////////////////////////////////////////////////////////////
 import { useEffect, useState } from 'react';
 import { fetchInvoicingData } from '../api/invoicingAPI';
 import styles from '../css/invoicing.module.css';
-import Sidebar from '../Components/Sidebar.jsx';
-import Header from '../Components/Header.jsx';
+
+import Header from '../components/Header.jsx';
 import { InvoicingCard } from '../Components/InvoicingCard.jsx';
+import Sidebar from '../components/Sidebar.jsx';
 
 export const InvoicingPage = () => {
   const [data, setData] = useState([]);
@@ -57,7 +22,7 @@ export const InvoicingPage = () => {
   return (
     <div className={` ${styles.hideScrollbar}`}>
       <div className={styles.wrapper}>
-        <Sidebar />
+        <Sidebar></Sidebar>
         <div className={styles.main}>
           <Header selectedFilter={selectedFilter} onFilterChange={handleFilterChange} />
           <div className={styles.container}>
